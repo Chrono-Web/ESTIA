108e23657c4b5ac1acc1a7b8bc69999e048ee7d4
Palu17 <paluellomattia@proton.me>
2026-08-24 00:44:16 +0200

    fix chat readability

--- apps/web/src/ui/icons/Icon.tsx
+++ apps/web/src/ui/icons/Icon.tsx
@@ -44,7 +44,7 @@ const ICONS = {
   eye: (
     <>
       <path d="M2.5 12S6 5.5 12 5.5 21.5 12 21.5 12 18 18.5 12 18.5 2.5 12 2.5 12Z" />
-      <circle cx="12" cy="12" r="3" />
+      <circle cx="12" cy="12" fill="currentColor" r="2.5" />
     </>
   ),
   "eye-off": (
